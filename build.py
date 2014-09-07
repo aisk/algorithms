c98db2e8a6f9b2435006669d03062e169ee8e3de
asaka <aisk1988@gmail.com>
2014-09-07 17:03:57 +0800

    add clean command in build.py

--- build.py
+++ build.py
@@ -5,6 +5,10 @@ import os
 import sys
 
 
+files = os.listdir('.')
+sources = [x for x in files if x.endswith('.cc')]
+
+
 def build(source):
     target = source[:-3]
     sys.stdout.write('compile [%(source)s] ... ' % locals())
@@ -15,11 +19,30 @@ def build(source):
         sys.stdout.write('failed!\n')
 
 
-def all():
-    files = os.listdir('.')
-    sources = [x for x in files if x.endswith('.cc')]
+def build_all():
     map(build, sources)
 
 
+def clean(source):
+    target = source[:-3]
+    sys.stdout.write('clean [%(source)s] ... ' % locals())
+    result = os.system('rm -f %(target)s' % locals())
+    if result == 0:
+        sys.stdout.write('done!\n')
+    else:
+        sys.stdout.write('failed!\n')
+
+
+def clean_all():
+    map(clean, sources)
+
+
 if __name__ == '__main__':
-    all()
+    if len(sys.argv) != 2:
+        print 'useage: build.py [build|clean|run]'
+    if sys.argv[1] == 'build':
+        build_all()
+    elif sys.argv[1] == 'clean':
+        clean_all()
+    else:
+        sys.exit(1)
